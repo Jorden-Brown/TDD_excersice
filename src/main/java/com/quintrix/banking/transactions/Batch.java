c97fe30ba21dc0dace82ad110552179b27b2ff92
Jorden-Brown <jordenbrown17@gmail.com>
2022-08-14 16:06:18 -0400

    g

---
+++ src/main/java/com/quintrix/banking/transactions/Batch.java
@@ -0,0 +1,17 @@
+package com.quintrix.banking.transactions;
+
+import java.util.Date;
+import java.util.Set;
+
+import com.quintrix.banking.DataModel;
+
+public class Batch extends DataModel  {
+
+	public boolean finalized;
+	public Set<Transaction> transactions;
+	public Date date;
+	
+	public boolean isFinal() {
+		return finalized;
+	}
+}
